58a7fbf05e494a89057d5c0e66f30dfca6de6912
Johan Martinsson <jm1974@hotmail.com>
2012-01-07 15:32:04 +0100

    skeleton project

---
+++ src/lean_challenge/core.clj
@@ -0,0 +1,1 @@
+(ns lean-challenge.core)
---
+++ test/lean_challenge/test/core.clj
@@ -0,0 +1,6 @@
+(ns lean-challenge.test.core
+  (:use [lean-challenge.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
