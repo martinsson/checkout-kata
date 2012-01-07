3380623832c46ee765435a8f933b8529601f6c6e
Johan Martinsson <jm1974@hotmail.com>
2012-01-07 16:13:50 +0100

    changed to clojure 1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject lean-challenge "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [midje "1.3.1"]]
+  :dev-dependencies [
+        [lein-midje "1.0.7"]])
