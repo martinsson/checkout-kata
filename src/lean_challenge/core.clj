7af36a4017d2fce7b2d453c397bdf3225dcb5402
Johan Martinsson <jm1974@hotmail.com>
2012-01-14 23:00:25 +0100

    iteration 4

--- src/lean_challenge/core.clj
+++ src/lean_challenge/core.clj
@@ -1,11 +1,14 @@
 (ns lean-challenge.core)
 
 (defn cost
-  [fruit] ({"b" 150 "a" 100 "c" 75} fruit))
+  [fruit] ({"b" 150 "a" 100 "p" 100 "m" 100 "c" 75} fruit))
 (defn csv-to-col [s]
   (seq (.split s ",")))
 (defn discount [fruits]
-  (* 20 (quot (count (filter #{"c"} fruits)) 2)))
+  (+ 
+    (* 20 (quot (count (filter #{"c"} fruits)) 2))
+    (* (cost "b") (quot (count (filter #{"b"} fruits)) 2)))
+  )
 (defn basket-price
   [fruits]
   (- (reduce + (map cost fruits)) 
--- test/lean_challenge/test/core.clj
+++ test/lean_challenge/test/core.clj
@@ -5,13 +5,18 @@
 (fact
   (cost "b" ) => 150
   (cost "a" ) => 100
-  (cost "c" ) => 75)
+  (cost "c" ) => 75
+  )
 
 (fact "adds up the cost of all fruits in the basket"
       (basket-price '("a" "c" "b")) => 325)
 
 (fact "discount for every two lots of cherries"
-      (basket-price '("c" "c")) => 130)
+      (basket-price '("c" "c")) => (- 150 20)
+      (basket-price '("c" "c" "c" "c")) => (- 300 40)
+      )
+(fact "buy two bananas get one for free"
+      (basket-price '("b" "b")) => (cost "b"))
 
 (fact 
   (csv-to-col "a,b,c") => '("a" "b" "c"))
