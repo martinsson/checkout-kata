38f6064248da5bf3f5f31a1d06f36a8208dadde4
Johan Martinsson <jm1974@hotmail.com>
2012-01-19 15:23:29 +0100

    added explanation of main

--- src/lean_challenge/main.clj
+++ src/lean_challenge/main.clj
@@ -1,6 +1,7 @@
 (ns lean-challenge.main
   (:use [lean-challenge.core] :reload))
 
+; runs in repl : just enter an empty line and you're ready for the next customer
 (loop [item (read-line) items []]
   (println (basket-price (concat (csv-to-col item) items)))
   (if (empty? item)
