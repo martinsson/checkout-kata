2b83490dff007157c358442576a6090535e1b8fd
Johan Martinsson <jm1974@hotmail.com>
2012-01-23 21:19:05 +0100

    extracted lots-of to improve readability of discount

--- src/lean_challenge/core.clj
+++ src/lean_challenge/core.clj
@@ -1,18 +1,23 @@
 (ns lean-challenge.core)
 
-(defn cost
-  [fruit] ({"b" 150 "a" 100 "p" 100 "m" 100 "c" 75} fruit))
+(defn cost [fruit] 
+  ({"b" 150 "a" 100 "p" 100 "m" 100 "c" 75} fruit))
+
 (defn csv-to-col [s]
   (seq (.split s ",")))
-(defn discount [fruits]
+
+(defn lots-of [lot-size basket & fruits]
+  (quot (count (filter (set fruits) basket)) lot-size))
+
+(defn discount [basket]
   (+ 
-    (* 20 (quot (count (filter #{"c"} fruits)) 2))
-    (* 50 (quot (count (filter #{"m"} fruits)) 2))
-    (* 100 (quot (count (filter #{"p"} fruits)) 3))
-    (* 100 (quot (count (filter #{"p" "a" "m"} fruits)) 4))
-    (* 200 (quot (count fruits) 5))
-    (* (cost "b") (quot (count (filter #{"b"} fruits)) 2)))
-  )
+    (* 20 (lots-of 2 basket "c"))
+    (* 50 (lots-of 2 basket "m"))
+    (* 100 (lots-of 3 basket "p"))
+    (* 150 (lots-of 2 basket "b"))
+    (* 100 (lots-of 4 basket "p" "a" "m"))
+    (* 200 (quot (count basket) 5))))
+
 (defn basket-price
   [fruits]
   (- (reduce + (map cost fruits)) 
