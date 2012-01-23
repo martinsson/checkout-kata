c31e02fa5fcdba9ee0e0dced49e544004cfcb70b
Johan Martinsson <jm1974@hotmail.com>
2012-01-23 21:24:53 +0100

    renames

--- src/lean_challenge/core.clj
+++ src/lean_challenge/core.clj
@@ -6,17 +6,17 @@
 (defn csv-to-col [s]
   (seq (.split s ",")))
 
-(defn lots-of [lot-size basket & fruits]
-  (quot (count (filter (set fruits) basket)) lot-size))
+(defn lots-of [lot-size fruits & discounted-fruits]
+  (quot (count (filter (set discounted-fruits) fruits)) lot-size))
 
-(defn discount [basket]
+(defn discount [fruits]
   (+ 
-    (* 20 (lots-of 2 basket "c"))
-    (* 50 (lots-of 2 basket "m"))
-    (* 100 (lots-of 3 basket "p"))
-    (* 150 (lots-of 2 basket "b"))
-    (* 100 (lots-of 4 basket "p" "a" "m"))
-    (* 200 (quot (count basket) 5))))
+    (* 20 (lots-of 2 fruits "c"))
+    (* 50 (lots-of 2 fruits "m"))
+    (* 100 (lots-of 3 fruits "p"))
+    (* 150 (lots-of 2 fruits "b"))
+    (* 100 (lots-of 4 fruits "p" "a" "m"))
+    (* 200 (quot (count fruits) 5))))
 
 (defn basket-price
   [fruits]
